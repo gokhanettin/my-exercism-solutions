a79f48e14971a2ee12588a34d490e50e8ab4b9ed
Gökhan Karabulut <gokhanettin@gmail.com>
2021-07-19 22:52:14 +0300

    Use any_of to check digit validity

--- cpp/trinary/trinary.cpp
+++ cpp/trinary/trinary.cpp
@@ -1,23 +1,16 @@
 #include "trinary.h"
 #include <numeric>
+#include <algorithm>
 
 namespace trinary {
 
 int to_decimal(const std::string& s)
 {
-    int decimal{0};
-    bool valid{true};
-
-    decimal = std::accumulate(cbegin(s), cend(s), 0,
-        [&valid](auto acc, auto c) {
-            int digit = c - '0';
-            if (digit > 3) valid = false;
-            return digit + acc * 3;
-        }
-    );
-
-    if (!valid) decimal = 0;
-    return decimal;
+    bool invalid = std::any_of(cbegin(s), cend(s),
+        [] (auto c) {return c < '0' ||  c > '2'; });
+    if (invalid) return 0;
+    return std::accumulate(cbegin(s), cend(s), 0,
+        [](auto acc, auto c) { return (c - '0') + acc * 3; });
 }
 
 }  // namespace trinary
