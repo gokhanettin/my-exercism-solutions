ed8876902bc888fdb15e5d240afffc65f72c2138
Gökhan Karabulut <gokhanettin@gmail.com>
2021-07-20 21:38:18 +0300

    Add rust reverse-string

---
+++ rust/reverse-string/src/lib.rs
@@ -0,0 +1,10 @@
+#[cfg(not(feature = "grapheme"))]
+pub fn reverse(input: &str) -> String {
+    input.chars().rev().collect()
+}
+
+#[cfg(feature = "grapheme")]
+pub fn reverse(input: &str) -> String {
+    use unicode_segmentation::UnicodeSegmentation;
+    UnicodeSegmentation::graphemes(input, true).rev().collect()
+}
