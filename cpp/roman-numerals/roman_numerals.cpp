7fb2c184078e9b2abc66f091dd9098ef808e1a6b
Gökhan Karabulut <gokhanettin@gmail.com>
2021-07-22 18:24:30 +0300

    Implement cpp roman-numerals without using unordered_map

--- cpp/roman-numerals/roman_numerals.cpp
+++ cpp/roman-numerals/roman_numerals.cpp
@@ -1,40 +1,39 @@
 #include "roman_numerals.h"
-#include <sstream>
 #include <string>
-#include <unordered_map>
+#include <utility>
+#include <vector>
 
 namespace roman_numerals {
 std::string convert(int number)
 {
-    std::unordered_map<int, std::stringstream> hash;
-    auto f = [](std::string&& s, int n, auto& hash) {
-        if (s.size() > 0) {
-            hash[n] << s[0];
-            hash[2 * n] << s[0] << s[0];
-            hash[3 * n] << s[0] << s[0] << s[0];
-        }
-        if (s.size() > 1) {
-            hash[4 * n] << s[0] << s[1];
-            hash[5 * n] << s[1];
-            hash[6 * n] << s[1] << s[0];
-            hash[7 * n] << s[1] << s[0] << s[0];
-            hash[8 * n] << s[1] << s[0] << s[0] << s[0];
-        }
-        if (s.size() > 2) {
-            hash[9 * n] << s[0] << s[2];
-        }
+    std::vector<std::pair<int, std::string>> pairs{
+        {1000, "M"},
+        {900, "CM"},
+        {500, "D"},
+        {400, "CD"},
+        {100, "C"},
+        {100, "C"},
+        {90, "XC"},
+        {50, "L"},
+        {40, "XL"},
+        {10, "X"},
+        {9, "IX"},
+        {8, "VIII"},
+        {7, "VII"},
+        {6, "VI"},
+        {5, "V"},
+        {4, "IV"},
+        {3, "III"},
+        {2, "II"},
+        {1, "I"},
     };
-
-    f("IVX", 1, hash);
-    f("XLC", 10, hash);
-    f("CDM", 100, hash);
-    f("M", 1000, hash);
-
     std::string ret{""};
-    for (auto i = 1; number; number /= 10, i *= 10) {
-        ret = hash[(number % 10) * i].str() + ret;
+    for (auto& pair : pairs) {
+        while (number >= pair.first) {
+            ret.append(pair.second);
+            number -= pair.first;
+        }
     }
-
     return ret;
 }
 } // namespace roman_numerals
