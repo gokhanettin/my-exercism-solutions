9d6ddaf5b042e5063cacb61f221b2a5a0e025855
Gokhan Karabulut <gokhan.karabulut@ulakhaberlesme.com.tr>
2021-10-29 22:01:27 +0300

    Add golang card-tricks exercise
    Practice slices and common operations over slices.

---
+++ go/card-tricks/card_tricks.go
@@ -0,0 +1,40 @@
+package cards
+
+// GetItem retrieves an item from a slice at given position. The second return value indicates whether
+// the given index exists in the slice or not.
+func GetItem(slice []int, index int) (int, bool) {
+	if index < 0 || index >= len(slice) {
+		return 0, false
+	}
+	return slice[index], true
+}
+
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range the value needs to be appended.
+func SetItem(slice []int, index, value int) []int {
+	if index < 0 || index >= len(slice) {
+		return append(slice, value)
+	}
+	slice[index] = value
+	return slice
+}
+
+// PrefilledSlice creates a slice of given length and prefills it with the given value.
+func PrefilledSlice(value, length int) []int {
+	if length < 0 {
+		return nil
+	}
+	slice := make([]int, length)
+	for i := range slice {
+		slice[i] = value
+	}
+	return slice
+}
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+	if index < 0 || index >= len(slice) {
+		return slice
+	}
+	return append(slice[:index], slice[index+1:]...)
+}
