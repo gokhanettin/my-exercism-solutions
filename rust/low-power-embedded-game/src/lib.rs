cdfa04e9a3b6cedf79f4098bbfea37404f3d2b13
Gokhan Karabulut <gokhan.karabulut@ulakhaberlesme.com.tr>
2022-04-30 19:25:55 +0300

    Add rust low-power-embedded-game

---
+++ rust/low-power-embedded-game/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    (dividend / divisor, dividend % divisor)
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    iter.enumerate()
+        .filter_map(|(i, v)| if i % 2 == 0 { Some(v) } else { None })
+}
+
+pub struct Position(pub i16, pub i16);
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+        self.0.abs() + self.1.abs()
+    }
+}
