24e746eb85cee1cd42e29637cb559ded1e6f4e84
Gokhan Karabulut <gokhan.karabulut@ulakhaberlesme.com.tr>
2022-05-01 18:09:05 +0300

    Add rust sublist with slice windows

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -10,14 +10,20 @@ pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison 
     if first_list == second_list {
         return Comparison::Equal;
     } else if first_list.len() > second_list.len() {
-        for i in 0..=first_list.len() - second_list.len() {
-            if &first_list[i..i + second_list.len()] == second_list {
+        if second_list.is_empty() {
+            return Comparison::Superlist;
+        }
+        for slice in first_list.windows(second_list.len()){
+            if slice == second_list {
                 return Comparison::Superlist;
             }
         }
-    } else if first_list.len() < second_list.len() {
-        for i in 0..=second_list.len() - first_list.len() {
-            if &second_list[i..i + first_list.len()] == first_list {
+    } else if first_list.len() < second_list.len()  {
+        if first_list.is_empty() {
+            return Comparison::Sublist;
+        }
+        for slice in second_list.windows(first_list.len()) {
+            if slice == first_list {
                 return Comparison::Sublist;
             }
         }
