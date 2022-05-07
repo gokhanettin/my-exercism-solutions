6edc815bd69e2b59c950ecd667a7c59a34bd9f74
Gokhan Karabulut <gokhan.karabulut@ulakhaberlesme.com.tr>
2022-05-08 01:51:58 +0300

    rust: high-scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,30 @@
+use std::cmp::Reverse;
+
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut scores: Vec<_> = self.scores().iter().copied().collect();
+        scores.sort_unstable_by_key(|&score| Reverse(score));
+        scores.into_iter().take(3).collect()
+    }
+}
