75164a9870990d39e13784a7750d2c338cc7318b
Gokhan Karabulut <gokhan.karabulut@ulakhaberlesme.com.tr>
2022-05-20 22:53:37 +0300

    rust: Add alphametics 3rd iteration

--- rust/alphametics/src/lib.rs
+++ rust/alphametics/src/lib.rs
@@ -1,65 +1,5 @@
 use std::collections::HashMap;
-use std::fmt;
-use std::slice;
-
-struct Permutation<'a, T>
-where
-    T: PartialEq + fmt::Debug,
-{
-    elements: &'a [T],
-    size: usize,
-    stack: Vec<State<'a, T>>,
-}
-
-type State<'a, T> = (Vec<&'a T>, slice::Iter<'a, T>);
-
-impl<'a, T> Permutation<'a, T>
-where
-    T: PartialEq + std::fmt::Debug,
-{
-    fn new(elements: &'a [T], size: usize) -> Self {
-        Permutation {
-            elements,
-            size,
-            stack: vec![(Vec::with_capacity(size), elements.iter())],
-        }
-    }
-}
-
-impl<'a, T> Iterator for Permutation<'a, T>
-where
-    T: PartialEq + std::fmt::Debug,
-{
-    type Item = Vec<&'a T>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        while let Some((mut path, mut options)) = self.stack.pop() {
-            path.pop();
-            while let Some(option) = options.next() {
-                if path.contains(&option) {
-                    continue;
-                }
-                path.push(option);
-                self.stack.push((path.clone(), options.clone()));
-                if path.len() == self.size {
-                    return Some(path);
-                }
-                let mut options = self.elements.iter();
-                while let Some(option) = options.next() {
-                    if path.contains(&option) {
-                        continue;
-                    }
-                    path.push(option);
-                    self.stack.push((path.clone(), options.clone()));
-                    if path.len() == self.size {
-                        return Some(path);
-                    }
-                }
-            }
-        }
-        None
-    }
-}
+use std::collections::HashSet;
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
     let mut power: i64 = -1;
@@ -84,71 +24,42 @@ pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
     let (_, leading) = letters.entry(prev).or_insert((0, false));
     *leading = true;
 
-    let digits = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let permutations = Permutation::new(digits, letters.len());
-    'perm: for permutation in permutations {
-        let mut sum = 0;
-        for (&digit, (&letter, &(coeff, leading))) in permutation.iter().zip(letters.iter()) {
-            match (digit, leading, letter, coeff) {
-                (0, true, _, _) => continue 'perm,
-                (digit, _, _, coeff) => {
-                    sum += digit * coeff;
-                }
+    let letters: Vec<_> = letters
+        .into_iter()
+        .map(|(letter, (coeff, leading))| (letter, coeff, leading))
+        .collect();
+    let digits: HashSet<i64> = (0i64..10).collect();
+    let mut stack = vec![(Vec::with_capacity(letters.len()), digits)];
+
+    while let Some((path, digits)) = stack.pop() {
+        if path.len() == letters.len() {
+            let sum: i64 = letters
+                .iter()
+                .map(|(_, coeff, _)| coeff)
+                .zip(path.iter())
+                .map(|(coeff, digit)| coeff * (*digit as i64))
+                .sum();
+            if sum == 0 {
+                return Some(
+                    letters
+                        .iter()
+                        .map(|&(letter, _, _)| letter)
+                        .zip(path.into_iter())
+                        .collect(),
+                );
             }
+            continue;
         }
-        if sum == 0 {
-            return Some(
-                letters
-                    .keys()
-                    .copied()
-                    .zip(permutation.iter().map(|&digit| *digit as u8))
-                    .collect(),
-            );
+        for digit in &digits {
+            if *digit == 0 && letters[path.len()].2 {
+                continue;
+            }
+            let mut path = path.clone();
+            let mut digits = digits.clone();
+            path.push(*digit as u8);
+            digits.remove(digit);
+            stack.push((path, digits))
         }
     }
     None
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_permutation_3_1() {
-        let mut permutations: Permutation<i32> = Permutation::new(&[1, 2, 3], 1);
-        assert_eq!(Some(vec![&1]), permutations.next());
-        assert_eq!(Some(vec![&2]), permutations.next());
-        assert_eq!(Some(vec![&3]), permutations.next());
-        assert_eq!(None, permutations.next());
-    }
-
-    #[test]
-    fn test_permutation_3_2() {
-        let mut permutations: Permutation<i32> = Permutation::new(&[1, 2, 3], 2);
-        assert_eq!(Some(vec![&1, &2]), permutations.next());
-        assert_eq!(Some(vec![&1, &3]), permutations.next());
-        assert_eq!(Some(vec![&2, &1]), permutations.next());
-        assert_eq!(Some(vec![&2, &3]), permutations.next());
-        assert_eq!(Some(vec![&3, &1]), permutations.next());
-        assert_eq!(Some(vec![&3, &2]), permutations.next());
-        assert_eq!(None, permutations.next());
-    }
-
-    #[test]
-    fn test_permutation_3_3() {
-        let mut permutations: Permutation<i32> = Permutation::new(&[1, 2, 3], 3);
-        assert_eq!(Some(vec![&1, &2, &3]), permutations.next());
-        assert_eq!(Some(vec![&1, &3, &2]), permutations.next());
-        assert_eq!(Some(vec![&2, &1, &3]), permutations.next());
-        assert_eq!(Some(vec![&2, &3, &1]), permutations.next());
-        assert_eq!(Some(vec![&3, &1, &2]), permutations.next());
-        assert_eq!(Some(vec![&3, &2, &1]), permutations.next());
-        assert_eq!(None, permutations.next());
-    }
-
-    #[test]
-    fn test_permutation_3_4() {
-        let mut permutations: Permutation<i32> = Permutation::new(&[1, 2, 3], 4);
-        assert_eq!(None, permutations.next());
-    }
 }
