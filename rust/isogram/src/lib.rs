4b5a0c8e7b5cf30917c45d7e4b449316a1dca1ec
Gokhan Karabulut <gokhan.karabulut@ulakhaberlesme.com.tr>
2022-05-21 02:05:24 +0300

    rust: Add isogram with hashset

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,14 +1,10 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub fn check(candidate: &str) -> bool {
+    let mut set = HashSet::new();
     candidate
         .bytes()
         .filter(|&b| b != b' ' && b != b'-')
         .map(|b| b.to_ascii_lowercase())
-        .fold(HashMap::new(), |mut acc, b| {
-            *acc.entry(b).or_insert(0) += 1;
-            acc
-        })
-        .iter()
-        .all(|(_, &count)| count == 1)
+        .all(|b| set.insert(b))
 }
