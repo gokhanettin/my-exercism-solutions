be68fb5f5c56420f7fdc3df80e7a8115ce42166d
Gokhan Karabulut <gokhan.karabulut@ulakhaberlesme.com.tr>
2022-05-29 18:16:34 +0300

    rust: Add two-bucket 2nd iteration

--- rust/two-bucket/src/lib.rs
+++ rust/two-bucket/src/lib.rs
@@ -1,9 +1,7 @@
 use std::cmp;
 use std::collections::{HashSet, VecDeque};
-use std::hash::{Hash, Hasher};
-use std::rc::Rc;
 
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+#[derive(PartialEq, Eq, Debug)]
 pub enum Bucket {
     One,
     Two,
@@ -30,163 +28,54 @@ pub fn solve(
 ) -> Option<BucketStats> {
     let mut queue = VecDeque::new();
     let mut set = HashSet::new();
-    let state = match start_bucket {
-        Bucket::One => State::new(capacity_1, 0, 1, capacity_1, capacity_2, *start_bucket),
-        Bucket::Two => State::new(0, capacity_2, 1, capacity_1, capacity_2, *start_bucket),
+
+    match start_bucket {
+        Bucket::One => {
+            queue.push_back(((capacity_1, 0), 1));
+            set.insert((0, capacity_2));
+        }
+        Bucket::Two => {
+            queue.push_back(((0, capacity_2), 1));
+            set.insert((capacity_1, 0));
+        }
     }
-    .unwrap();
 
-    set.insert(Rc::clone(&state));
-    queue.push_back(state);
-
-    while let Some(state) = queue.pop_front() {
-        let (bucket_1, bucket_2) = state.buckets;
+    while let Some(((bucket_1, bucket_2), moves)) = queue.pop_front() {
         if bucket_1 == goal {
             return Some(BucketStats {
-                moves: state.moves,
+                moves,
                 goal_bucket: Bucket::One,
                 other_bucket: bucket_2,
             });
         } else if bucket_2 == goal {
             return Some(BucketStats {
-                moves: state.moves,
+                moves,
                 goal_bucket: Bucket::Two,
                 other_bucket: bucket_1,
             });
         } else {
-            for function in FUNCTIONS {
-                if let Some(state) = function(&state) {
-                    if !set.contains(&state) {
-                        set.insert(Rc::clone(&state));
-                        queue.push_back(state);
-                    }
+            let pour_1 = cmp::min(capacity_2 - bucket_2, bucket_1);
+            let pour_2 = cmp::min(capacity_1 - bucket_1, bucket_2);
+            let states = [
+                // Empty
+                (0, bucket_2),
+                (bucket_1, 0),
+                // Fill
+                (capacity_1, bucket_2),
+                (bucket_1, capacity_2),
+                // Pour
+                (bucket_1 - pour_1, bucket_2 + pour_1),
+                (bucket_1 + pour_2, bucket_2 - pour_2),
+            ];
+
+            for state in states {
+                if set.contains(&state) {
+                    continue;
                 }
+                set.insert(state);
+                queue.push_back((state, moves + 1));
             }
         }
     }
     None
 }
-
-const FUNCTIONS: [for<'r> fn(&'r State) -> Option<Rc<State>>; 6] = [
-    State::fill_1,
-    State::fill_2,
-    State::empty_1,
-    State::empty_2,
-    State::pour_1,
-    State::pour_2,
-];
-
-#[derive(Debug)]
-struct State {
-    moves: u8,
-    start_bucket: Bucket,
-    buckets: (u8, u8),
-    capacities: (u8, u8),
-}
-
-impl State {
-    fn new(
-        bucket_1: u8,
-        bucket_2: u8,
-        moves: u8,
-        capacity_1: u8,
-        capacity_2: u8,
-        start_bucket: Bucket,
-    ) -> Option<Rc<State>> {
-        if bucket_1 > capacity_1 || bucket_2 > capacity_2 {
-            None
-        } else if start_bucket == Bucket::One && bucket_2 == capacity_2 && bucket_1 == 0 {
-            None
-        } else if start_bucket == Bucket::Two && bucket_1 == capacity_1 && bucket_2 == 0 {
-            None
-        } else {
-            Some(Rc::new(State {
-                moves,
-                start_bucket,
-                buckets: (bucket_1, bucket_2),
-                capacities: (capacity_1, capacity_2),
-            }))
-        }
-    }
-
-    fn fill_1(&self) -> Option<Rc<State>> {
-        State::new(
-            self.capacities.0,
-            self.buckets.1,
-            self.moves + 1,
-            self.capacities.0,
-            self.capacities.1,
-            self.start_bucket,
-        )
-    }
-
-    fn fill_2(&self) -> Option<Rc<State>> {
-        State::new(
-            self.buckets.0,
-            self.capacities.1,
-            self.moves + 1,
-            self.capacities.0,
-            self.capacities.1,
-            self.start_bucket,
-        )
-    }
-
-    fn empty_1(&self) -> Option<Rc<State>> {
-        State::new(
-            0,
-            self.buckets.1,
-            self.moves + 1,
-            self.capacities.0,
-            self.capacities.1,
-            self.start_bucket,
-        )
-    }
-
-    fn empty_2(&self) -> Option<Rc<State>> {
-        State::new(
-            self.buckets.0,
-            0,
-            self.moves + 1,
-            self.capacities.0,
-            self.capacities.1,
-            self.start_bucket,
-        )
-    }
-
-    fn pour_1(&self) -> Option<Rc<State>> {
-        let pour = cmp::min(self.capacities.1 - self.buckets.1, self.buckets.0);
-        State::new(
-            self.buckets.0 - pour,
-            self.buckets.1 + pour,
-            self.moves + 1,
-            self.capacities.0,
-            self.capacities.1,
-            self.start_bucket,
-        )
-    }
-    fn pour_2(&self) -> Option<Rc<State>> {
-        let pour = cmp::min(self.capacities.0 - self.buckets.0, self.buckets.1);
-        State::new(
-            self.buckets.0 + pour,
-            self.buckets.1 - pour,
-            self.moves + 1,
-            self.capacities.0,
-            self.capacities.1,
-            self.start_bucket,
-        )
-    }
-}
-
-impl Hash for State {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.buckets.hash(state);
-    }
-}
-
-impl PartialEq for State {
-    fn eq(&self, other: &Self) -> bool {
-        self.buckets == other.buckets
-    }
-}
-
-impl Eq for State {}
